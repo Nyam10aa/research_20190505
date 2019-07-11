b92de081d42b917d0743aa9c18c4d051244d1e34
Nyam10aa <nyam10aa@gmail.com>
2019-07-11 22:18:22 +0900

    20190711

--- python_shell_predict.py
+++ python_shell_predict.py
@@ -4,7 +4,15 @@ import sys
 #start = int(sys.argv[1])
 #end = int(sys.argv[2])
 
-for model in range(1,724):
+#------------1Model multiTest------------------
+# for model in range(1,724):
+# 	#for startt in range(1,723):
+# 	print(model)
+# 	os.system("time python predict_3class_keras_by_one_1Model_multiTest.py %d %d %d divided_by_20_results/result_0702_723x723.csv" % (model, 1, 723))
+
+
+#------------multiModel 1Test------------------
+for start in range(1,724):
 	#for startt in range(1,723):
-	print(model)
-	os.system("time python predict_3class_keras_by_one_1Model_multiTest.py %d %d %d divided_by_20_results/result_0702_723x723.csv" % (model, 1, 723))
+	print(start)
+	os.system("time python predict_3class_keras_by_one_1Model_multiTest.py %d %d divided_by_20_results/result_0711_723x723.csv" % (start, start))
