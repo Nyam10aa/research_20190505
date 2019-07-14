0ecae452b8355412191e9245c847a45fa86c1926
Nyam10aa <nyam10aa@gmail.com>
2019-07-15 00:03:13 +0900

    20190714

--- predict_3class_keras_by_one_multiModel_1Test.py
+++ predict_3class_keras_by_one_multiModel_1Test.py
@@ -19,12 +19,17 @@ startt = int(sys.argv[1])
 endd = int(sys.argv[2])
 output = sys.argv[3]
 
-#end = start
+
+models = []
+for model_type in range(1, 26):
+    model = keras.models.load_model("divided_by_20_keras_model/by_one/model_case_%d.h5" % model_type, compile=False)
+    model.compile(loss="categorical_crossentropy",optimizer=SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
+    models.append(model)
+    print("="*10, model_type, "="*10)
+    elapsed_time = time.time() - start
+    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
 
-
-#x_test_total = np.load("divided_by_20/datas_%03d_%03d.npy" % (start, start))
-#y_test_total = np.load("divided_by_20/labels_%03d_%03d.npy" % (start, start))
 
 for start in range(startt, endd+1):
     x_test_total = np.load(read_from_s3('processed_data_STD/', 'datas_%03d_%03d.npy' % (start, start)))
--- predict_get_runtime.py
+++ predict_get_runtime.py
@@ -19,18 +19,23 @@ def read_from_s3(file_path, file_name):
         return io.BytesIO(obj['Body'].read())
 
 
-models = []
-for model_type in range(1, 724):
-    model = keras.models.load_model("divided_by_20_keras_model/by_one/model_case_%d.h5" % model_type, compile=False)
-    model.compile(loss="categorical_crossentropy",optimizer=SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
-    models.append(model)
-    print("="*10, model_type, "="*10)
-    elapsed_time = time.time() - start
-    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+# models = []
+# for model_type in range(1, 724):
+#     model = keras.models.load_model("divided_by_20_keras_model/by_one/model_case_%d.h5" % model_type, compile=False)
+#     model.compile(loss="categorical_crossentropy",optimizer=SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
+#     models.append(model)
+#     print("="*10, model_type, "="*10)
+#     elapsed_time = time.time() - start
+#     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
+
+x_datas = []
+y_datas = []
 for start in range(1, 724):
     x_test_total = np.load(read_from_s3('processed_data_STD/', 'datas_%03d_%03d.npy' % (start, start)))
     y_test_total = np.load(read_from_s3('processed_data_STD/', 'labels_%03d_%03d.npy' % (start, start)))
+    x_datas.append(x_test_total)
+    y_test_total.append(y_test_total)
     print("="*10, start, "="*10)
     elapsed_time = time.time() - start
     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
