b2aa0160f45e09f263df76924ba844cfb0c92d6f
Ubuntu <ubuntu@ip-172-31-18-85.ap-northeast-1.compute.internal>
2019-09-04 08:46:41 +0000

    20190904

--- python_shell_s3_upload.py
+++ python_shell_s3_upload.py
@@ -1,16 +1,16 @@
 import boto3
 
 def upload():
-	#path = './divided_by_20_keras_model/by_one/'
-	path = './divided_by_20/'
+	path = './divided_by_20_keras_model/by_one/'
+	#path = './divided_by_20/'
 	s3 = boto3.resource('s3')
 	bucket = s3.Bucket('takenaka')
-	#bucket_path = 'model_STD/'
-	bucket_path = 'processed_data_STD/'
+	bucket_path = 'model_STD_1_723/'
+	#bucket_path = 'processed_data_STD/'
 	try:
-		for i in range(301,800):
-			#file_stream = 'model_case_%d.h5' % i
-			file_stream = 'datas_%03d_%03d.npy' % (i, i)
+		for i in range(1,724):
+			file_stream = 'model_case_%d.h5' % i
+			#file_stream = 'datas_%03d_%03d.npy' % (i, i)
 			bucket.upload_file(path +file_stream, bucket_path + file_stream)
 			print(path+file_stream)
 	except:
